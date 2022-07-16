1a4d5dab69a84f8b4f20066ee5c58b8d161fffd7
Miroslav <mir19972905@mai.ru>
2022-07-16 11:28:12 +0300

    task47

---
+++ task47/Program.cs
@@ -0,0 +1,16 @@
+﻿
+int M = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
+double [,] array = new double [M, N];
+
+Random rnd = new Random();
+
+for (int i = 0; i < M; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        array[i, j] = rnd.NextDouble();
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine(); 
+}
