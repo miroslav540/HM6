1bfd7fe6c726c34ae7977279653f9e44cb9ebd40
Miroslav <mir19972905@mai.ru>
2022-07-16 11:28:27 +0300

    task50

---
+++ task50/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Enter M and N");
+int M = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("enter i0 and j0"); 
+int i0 = Convert.ToInt32(Console.ReadLine());
+int j0 = Convert.ToInt32(Console.ReadLine());
+
+
+double [,] array = new double [M, N];
+
+Random rnd = new Random();
+
+for (int i = 0; i < M; i++){
+    for (int j = 0; j < N; j++){
+        array[i, j] = rnd.Next(-10, 10);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine(); 
+}
+
+
+if (i0 >= M || j0 >= N || i0 < 0 || j0 < 0)
+{
+    Console.WriteLine("Out of range"); 
+}
+else {
+    Console.WriteLine("Element with j=j0 and i=i0 is {0}", array[i0,j0]);
+}
